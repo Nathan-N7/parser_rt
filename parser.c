2d75393e640f2217e99f239df1be0c4ec2676969
Nathan-N7 <nathan.rod1710@gmail.com>
2025-08-31 18:27:52 -0300

    ler prefixo e linhas vazias

--- parser.c
+++ parser.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+#include "gnl/get_next_line.h"
 
 int	verify_file(char *file)
 {
@@ -28,12 +29,43 @@ int	verify_file(char *file)
 	return (fd);
 }
 
+void	read_file(int fd)
+{
+	int	i;
+	char	*str;
+
+	str = get_next_line(fd);
+	while (str)
+	{
+		i = 0;
+		while (str[i] == 32 || (str[i] > 6 && str[i] < 14))
+			i++;
+		if ((str[i] > 96 && str[i] < 123) || (str[i] > 64 && str[1] < 91))
+		{
+			if (str[i] == 'A')
+				printf("Luz ambiente 👍\n");
+			else if (str[i] == 'C')
+				printf("Camera 👍\n");
+			else if (str[i] == 'L')
+				printf("Luz 👍\n");
+			else if (str[i] == 's' && str[i + 1] == 'p')
+				printf("Esfera 👍\n");
+			else if (str[i] == 'p' && str[i + 1] == 'l')
+				printf("Plano 👍\n");
+			else if (str[i] == 'c' && str[i + 1] == 'y')
+				printf("Cilindro 👍\n");
+		}
+		free(str);
+		str = get_next_line(fd);
+	}
+}
+
 void	parser_rt(char *file)
 {
 	int	fd;
 
 	fd = verify_file(file);
-	printf("deu certo paizao 👍: %d", fd);
+	read_file(fd);
 	close (fd);
 }
 
@@ -41,6 +73,6 @@ void	parser_rt(char *file)
 int main(int ac, char **av)
 {
 	if (ac != 2)
-		printf("ta errado isso ai");
+		return (printf("ta errado isso ai"), 1);
 	parser_rt(av[1]);
 }
